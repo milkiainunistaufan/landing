3db3a83cf7c7c00f0981c1fa0a890c970045089f
unknown <milkiainun@gmail.com>
2023-04-15 01:17:09 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary : '#4d4d4d',
+        secondary: '#64748b',
+        dark : '#0f172a',
+      },
+      screens: {
+        '2xl': '1320px',
+        '2xl': '1366px',
+        '2xl' : '1440px'
+      },
+    },
+  },
+  plugins: [],
+}
+
